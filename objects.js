32d5861cf011aaba9aefa1bea37e919d68c0ff73
epic-constructor-9765 <barton.e.marks@gmail.com>
2019-07-26 12:24:10 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { 
+  olp: "is anybody home",
+  riahanna: "diamonds",
+  galantis: "emoji"
+ };
+  
+  function updatePlaylist (playlist, artistName, songTitle) {
+    
+  }
+  
+  function updateObjectWithObject(targetObject, updatesObject) {
+  return Object.assign({}, playlist, updatesObject)
+}
